81851ded578136001cfcd1d81f99509b7198e28c
amin jmil <aminjmil7@gmail.com>
2021-11-13 23:21:37 +0100

    fix issues

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/project-voice-admin-panel'));
+app.use(express.static('./dist/ProjectVoice-AdminPanel'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/project-voice-admin-panel/'}),
+    res.sendFile('index.html', {root: 'dist/ProjectVoice-AdminPanel/'}),
 );
 
 // Start the app by listening on the default Heroku port
